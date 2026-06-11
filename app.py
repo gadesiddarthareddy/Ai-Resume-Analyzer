e5eaf34e5e4750c2e22007b8762db75b124c8c4e
gadesiddarthareddy <gade.siddarthareddy@gmail.com>
2026-06-11 23:24:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-streamlit
+import streamlit as st
+
+st.title("AI Resume Analyzer")
+
+name = st.text_input("Enter your name")
+
+resume = st.file_uploader("Upload Resume", type=["pdf"])
+
+if st.button("Analyze"):
+
+    if resume:
+        st.success("Resume uploaded successfully!")
+
+        st.write("Name:", name)
+        st.write("Resume Name:", resume.name)
+        st.write("Resume Size:", round(resume.size/1024, 2), "KB")
+
+        st.subheader("Resume Score")
+        st.progress(75)
+        st.write("Your resume score: 75/100")
+
+    else:
+        st.warning("Please upload a resume.")
